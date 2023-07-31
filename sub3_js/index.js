16b3055c52966c68eea65636fbb4af11caf0bdda
digitopia01 <140796008+digitopia01@users.noreply.github.com>
2023-07-31 20:45:59 +0900

    added sub3.html file and css, js, image folders

---
+++ sub3_js/index.js
@@ -0,0 +1,3 @@
+function gotomall(){
+    location.href = 'https://brand.naver.com/downykr/category/ebc8cdc852e54e39ac3ef39b3c04f4a1?cp=1&gclid=CjwKCAjwlJimBhAsEiwA1hrp5uPbWxpMW7onnmOW0GQ9gB28WcD52YPQsVvphC58ZtNpI6ydZ2ucthoCx-AQAvD_BwE';
+}
